harshpreet93/CS153

commit 8616f552dec5114d9780957dc670f41595e494e6

JavaWordToken is for parsing java reserved words

Assignment 2/src/wci/frontend/java/tokens/JavaWordToken.java
@@ -1,0 +1,54 @@
+package wci.frontend.java.tokens;
+
+import wci.frontend.*;
+import wci.frontend.java.*;
+
+import static wci.frontend.java.JavaTokenType.*;
+
+/**
+ * <h1>JavaWordToken</h1>
+ *
+ * <p> Java word tokens (identifiers and reserved words).</p>
+ *
+ * <p>Copyright (c) 2009 by Ronald Mak</p>
+ * <p>For instructional purposes only.  No warranties.</p>
+ */
+public class JavaWordToken extends JavaToken
+{
+    /**
+     * Constructor.
+     * @param source the source from where to fetch the token's characters.
+     * @throws Exception if an error occurred.
+     */
+    public JavaWordToken(Source source)
+            throws Exception
+    {
+        super(source);
+    }
+
+    /**
+     * Extract a Java word token from the source.
+     * @throws Exception if an error occurred.
+     */
+    protected void extract()
+            throws Exception
+    {
+        StringBuilder textBuffer = new StringBuilder();
+        char currentChar = currentChar();
+
+        // Get the word characters (letter or digit).  The scanner has
+        // already determined that the first character is a letter.
+        while (Character.isLetterOrDigit(currentChar)) {
+            textBuffer.append(currentChar);
+            currentChar = nextChar();  // consume character
+        }
+
+        text = textBuffer.toString();
+
+        // Java reserved words are all lower case
+        // Is it a reserved word or an identifier?
+        type = (RESERVED_WORDS.contains(text.toLowerCase()))
+                ? JavaTokenType.valueOf(text.toLowerCase())  // reserved word
+                : IDENTIFIER;                                  // identifier
+    }
+}

commit 60f30405764214bf12d1a7c4527ae3ba4228955e

Fixes: 8616f552dec5 ("JavaWordToken is for parsing java reserved words")
The aforementioned commit tries to convert the text of a reserved word token to that of its corresponding enumerated type, but in doing so it first converts the token-text to lower case. All enumerators have all uppercase names by default, and as such the call to JavaTokenType.valueOf() fails saying it cannot find the enumerated type in question. For example: if JavaWordToken encounters the "public" reserved word token, it will convert it to lowercase (which it already is) and then call JavaTokenType.valueOf(), at which point JavaTokenType will look through its values for a value with the name "public" and fail to find it because the name of JavaTokenType.PUBLIC is not "public", it is "PUBLIC".

Assignment 2/src/wci/frontend/java/tokens/JavaWordToken.java
@@ -48,7 +48,7 @@ public class JavaWordToken extends JavaToken
         // Java reserved words are all lower case
         // Is it a reserved word or an identifier?
         type = (RESERVED_WORDS.contains(text.toLowerCase()))
-                ? JavaTokenType.valueOf(text.toLowerCase())  // reserved word
+                ? JavaTokenType.valueOf(text.toUpperCase())  // reserved word
                 : IDENTIFIER;                                  // identifier
     }
 }